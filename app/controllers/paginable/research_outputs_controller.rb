cffb456ba0c68efcb571124dba4a4db5f465ffa4
aaronskiba <askiba@ualberta.ca>
2024-11-12 11:10:41 -0700

    Change`research_outputs` to local variable
    The corresponding view, `app/views/paginable/research_outputs/_index.html.erb`, uses `scope` rather than `@research_outputs`.

--- app/controllers/paginable/research_outputs_controller.rb
+++ app/controllers/paginable/research_outputs_controller.rb
@@ -10,13 +10,13 @@ module Paginable
     # GET /paginable/plans/:plan_id/research_outputs
     def index
       @plan = Plan.find_by(id: params[:plan_id])
-      # Same @research_outputs assignment as app/controllers/research_outputs_controller.rb
-      @research_outputs = ResearchOutput.includes(:repositories).where(plan_id: @plan.id)
+      # Same assignment as app/controllers/research_outputs_controller.rb
+      research_outputs = ResearchOutput.includes(:repositories).where(plan_id: @plan.id)
       # Same authorize handling as app/controllers/research_outputs_controller.rb
-      authorize @research_outputs.first || ResearchOutput.new(plan_id: @plan.id)
+      authorize research_outputs.first || ResearchOutput.new(plan_id: @plan.id)
       paginable_renderise(
         partial: 'index',
-        scope: @research_outputs,
+        scope: research_outputs,
         query_params: { sort_field: 'research_outputs.title' },
         format: :json
       )
